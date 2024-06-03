441b314dc3b0d0fff8167df3dc18541f56ea8b62
Naresh D Bhadresha <nareshjangid84@gmail.com>
2024-06-03 18:38:52 +0530

    Initial Commit

---
+++ TMDBKit/TMDBKit.h
@@ -0,0 +1,18 @@
+//
+//  TMDBKit.h
+//  TMDBKit
+//
+//  Created by Naresh Bhadresha on 03/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TMDBKit.
+FOUNDATION_EXPORT double TMDBKitVersionNumber;
+
+//! Project version string for TMDBKit.
+FOUNDATION_EXPORT const unsigned char TMDBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDBKit/PublicHeader.h>
+
+
